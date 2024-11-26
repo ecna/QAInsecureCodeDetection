6d9e5fe4f4a6ffa61b8dae1eaf2bf0b46565c361
Richard Holleman <37831407+ecna@users.noreply.github.com>
2024-11-26 21:05:07 +0100

    added wait till page is loaded

--- src/contentScript.tsx
+++ src/contentScript.tsx
@@ -1,59 +1,61 @@
 import { getCodeSnippets } from "./contentPageContol";
 
-setTimeout(() => {
 
-  var codeSnippert = getCodeSnippets() ;
+window.addEventListener("load", myMain, false);
 
-  chrome.runtime.sendMessage({ action: "checkCodeCPP", code: codeSnippert }, (response) => {
-    // Got an asynchronous response with the data from the background script
-    if (response.action === "codeCheckedCPP") {
-      const codeBlocks = document.querySelectorAll('pre code');
+function myMain() {
 
-      for (let i = 0; i < codeBlocks.length; i++) {
-        const result = response.results[i];
+    var codeSnippert = getCodeSnippets();
 
-        const codeElement = codeBlocks[i] as HTMLElement;
-        addCheckMark(codeElement, result);
+    chrome.runtime.sendMessage({ action: "checkCodeCPP", code: codeSnippert }, (response) => {
+      // Got an asynchronous response with the data from the background script
+      if (response.action === "codeCheckedCPP") {
+        const codeBlocks = document.querySelectorAll('pre code');
+
+        for (let i = 0; i < codeBlocks.length; i++) {
+          const result = response.results[i];
+
+          const codeElement = codeBlocks[i] as HTMLElement;
+          addCheckMark(codeElement, result);
+        }
       }
-    }
-  });
+    });
 
-  setTimeout(() => {
-  chrome.runtime.sendMessage({ action: "checkCodeSecure", code: codeSnippert }, (response) => {
-    // Got an asynchronous response with the data from the background script
-    if (response.action === "codeCheckedSecure") {
-      const codeBlocks = document.querySelectorAll('pre code');
+    setTimeout(() => {
+      chrome.runtime.sendMessage({ action: "checkCodeSecure", code: codeSnippert }, (response) => {
+        // Got an asynchronous response with the data from the background script
+        if (response.action === "codeCheckedSecure") {
+          const codeBlocks = document.querySelectorAll('pre code');
 
-      for (let i = 0; i < codeBlocks.length; i++) {
-        const result = response.results[i];
+          for (let i = 0; i < codeBlocks.length; i++) {
+            const result = response.results[i];
 
-        console.log("Result:", result);
-      }
-    }
-  });
-},200);
+            console.log("Result:", result);
+          }
+        }
+      });
+    }, 200);
 
+  function addCheckMark(codeElement: HTMLElement, isCpp: boolean) {
+    const checkMark = document.createElement('div');
+    checkMark.style.top = '5px';
+    checkMark.style.right = '5px';
+    checkMark.style.fontSize = '20px';
+    checkMark.style.fontWeight = 'bold';
+    checkMark.style.float = "right";
+    checkMark.style.color = isCpp ? 'green' : 'red';
+    checkMark.textContent = isCpp ? '✓' : '✗';
+    if (codeElement.parentNode) codeElement.parentNode.insertBefore(checkMark, codeElement);
+  }
 
-}, 500);
+  // chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+  //   if (msg.color) {
+  //     console.log("Receive color = " + msg.color);
+  //     document.body.style.backgroundColor = msg.color;
+  //     sendResponse("Change color to " + msg.color);
+  //   } else {
+  //     sendResponse("Color message is none.");
+  //   }
+  // });
 
-function addCheckMark(codeElement: HTMLElement, isCpp: boolean) {
-  const checkMark = document.createElement('div');
-  checkMark.style.top = '5px';
-  checkMark.style.right = '5px';
-  checkMark.style.fontSize = '20px';
-  checkMark.style.fontWeight = 'bold';
-  checkMark.style.float = "right";
-  checkMark.style.color = isCpp ? 'green' : 'red';
-  checkMark.textContent = isCpp ? '✓' : '✗';
-  if (codeElement.parentNode) codeElement.parentNode.insertBefore(checkMark, codeElement);
 }
-
-// chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-//   if (msg.color) {
-//     console.log("Receive color = " + msg.color);
-//     document.body.style.backgroundColor = msg.color;
-//     sendResponse("Change color to " + msg.color);
-//   } else {
-//     sendResponse("Color message is none.");
-//   }
-// });
